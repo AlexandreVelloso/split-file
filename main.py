cfa272961b2a7585004836c1380ac6ca54d8ebcf
Alexandre Velloso <xande.velloso@gmail.com>
2022-06-16 00:22:07 -0300

    Create interface

--- main.py
+++ main.py
@@ -2,40 +2,99 @@ from fileinput import filename
 import PySimpleGUI as sg
 import os.path
 
+from inputs import get_time_in_seconds
+from split_file import split_file
+
 file_picker = [
     [
-        sg.Text("Please select a file"),
-        sg.In(key="file_path", size=(30, 1), enable_events=True),
-        sg.FileBrowse(file_types=(("*.mp3"),)),
+        sg.Text('Please select a file'),
+        sg.In(key='file_path', size=(100, 1), disabled=True),
+        sg.FileBrowse(initial_folder='./Files', file_types=[('Mp3 files', '*.mp3')]),
     ]
 ]
 
-file_viewer = [
-    [sg.Text("File name")],
-    [sg.Text(size=(100, 1), key="file_viewer")],
+parameters = [
+    [
+        sg.Text('Enter the first part number'),
+        sg.InputText('1', key='part_number', size=(3, 1)),
+    ],
+    [
+        sg.Text('Enter the part prefix'),
+        sg.InputText('', key='part_prefix', size=(30, 1)),
+    ],
+    [
+        sg.Text('Enter the separator'),
+        sg.InputText('-', key='separator', size=(5, 1)),
+    ],
+    [
+        sg.Text('Enter the file duration'),
+        sg.InputText('00:00:00', key='file_duration', size=(10, 1)),
+    ],
+    [
+        sg.Text('Enter the part duration'),
+        sg.InputText('05:00', key='part_duration', size=(10, 1)),
+    ],
+    [
+        sg.Text('Enter the time of each chapter'),
+        sg.Multiline(size=(30, 10), key='chapters'),
+    ]
 ]
 
 split_file_button = [
-    [sg.Button("Split file")],
+    [sg.Button('Split file', key='split_file')],
 ]
 
 layout = [
     [
         file_picker,
-        file_viewer,
+        parameters,
+    ],
+    [
+        sg.HorizontalSeparator(),
+    ],
+    [
         split_file_button,
     ]
 ]
 
-window = sg.Window("Split audio file", layout)
+window = sg.Window('Split audio file', layout)
+
+
+def split():
+    filename = values['file_path'].rstrip().replace(' ', '\\ ')
+    filename = os.path.basename(filename)
+
+    part_prefix = values['part_prefix']
+    separator = values['separator']
+
+    file_total_duration_seconds = get_time_in_seconds(values['file_duration'])
+    part_duration_seconds = get_time_in_seconds(values['part_duration'])
+    start_part_number = int(values['part_number'])
+
+    chapters = values['chapters'].split('\n')
+
+    part_number = int(start_part_number)
+
+    split_file(filename, part_prefix, separator, file_total_duration_seconds, part_duration_seconds, part_number, chapters)
+
 
 # event loop
 while True:
     event, values = window.read()
-    if event == sg.WIN_CLOSED or event == "Exit":
+    if event == sg.WIN_CLOSED or event == 'Exit':
         break
-    if event == "file_path":
-        filename = values["file_path"]
-        window['file_viewer'].update(filename)
+
+    if event == 'split_file':
+        filename = values['file_path']
+
+        if filename == '':
+            sg.popup('Please select a file')
+            continue
+
+        if not os.path.isfile(filename):
+            sg.popup('Please select a valid file')
+            continue
+
+        split()
 
 window.close()
--- split_file.py
+++ split_file.py
@@ -1,18 +1,19 @@
 import os
-from inputs import *
 from progress_bar import *
 
+from inputs import get_time_in_seconds
 
-def cut_file(filename, output_name, start, end, file_extension="mp3"):
-    input_path = os.path.abspath(f"Files/{filename}")
-    output_path = os.path.abspath(f"Parts/{output_name}.{file_extension}")
 
-    command = f"ffmpeg -y -i {input_path} -ss {start} -t {end - start} -acodec copy {output_path} > /dev/null 2>&1"
+def cut_file(filename, output_name, start, end, file_extension='mp3'):
+    input_path = os.path.abspath(f'Files/{filename}')
+    output_path = os.path.abspath(f'Parts/{output_name}.{file_extension}')
+
+    command = f'ffmpeg -y -i {input_path} -ss {start} -t {end - start} -acodec copy {output_path} > /dev/null 2>&1'
 
     os.system(command)
 
 
-def split_file(filename, part_prefix, separator, part_suffix, file_total_duration, time_start_file = 0, start_number=1, duration=300): 
+def split_chapter(filename, part_prefix, separator, part_suffix, file_total_duration, time_start_file = 0, start_number=1, duration=300): 
     part_number = start_number
     start = time_start_file
     end = time_start_file + duration
@@ -40,85 +41,20 @@ def split_file(filename, part_prefix, separator, part_suffix, file_total_duratio
     return part_number
 
 
-def read_inputs():
-    filename = read_file_name('Enter the file name: ')
+def split_file(filename, part_prefix, separator, file_total_duration_seconds, part_duration_seconds, start_part_number, chapters):
+    part_number = int(start_part_number)
 
-    start_part_number = int(read_input('Enter the start part number (default 1): ', 1))
+    for i in range(0, len(chapters)):
+        chapter = chapters[i]
 
-    part_prefix = read_input('Enter a part prefix value: ')
-    part_suffix = read_input('Enter a suffix: ', '')
+        chapter_words = chapter.split(' ')
+        time_start_file = get_time_in_seconds(chapter_words[0])
+        chapter_name = '\\ '.join(chapter_words[1:])
 
-    part_prefix = part_prefix.replace(' ', '\\ ')
-    part_suffix = part_suffix.replace(' ', '\\ ')
+        if i == len(chapters) - 1:
+            time_end_file = file_total_duration_seconds
+        else:
+            next_chapter = chapters[i + 1]
+            time_end_file = get_time_in_seconds(next_chapter.split(' ')[0])
 
-    time_start_file = read_time_in_seconds('Enter the time to start cutting (default 00:00): ', '00:00')
-
-    file_total_duration_seconds = read_time_in_seconds('Enter file total duration (e.g. 10:05:20): ')
-    part_duration_seconds = read_time_in_seconds('Enter the duration of the parts (default 05:00): ', '05:00')
-
-    confirm_text = f"""\nFile name: {filename}
-    Start part number: {start_part_number}
-    Part prefix value: {part_prefix}
-    Part suffix value: {part_suffix}
-    Time to start cutting: {time_start_file}
-    File total duration in seconds: {file_total_duration_seconds}
-    Part duration in seconds: {part_duration_seconds}
-
-    Are those values correct? (Y/n): """
-
-    confirm = input(confirm_text)
-
-    if (confirm == 'n' or confirm == 'N'):
-        exit()
-    
-    print('')
-    split_file(filename, part_prefix, '-', part_suffix, file_total_duration_seconds, time_start_file, start_part_number, part_duration_seconds)
-
-
-def read_parameters_file():
-    with open("parameters.txt") as fp:
-        filename = fp.readline().rstrip().replace(' ', '\\ ')
-        start_part_number = fp.readline().rstrip()
-        part_prefix = fp.readline().rstrip().replace(' ', '\\ ')
-        separator = fp.readline().rstrip().replace(' ', '\\ ')
-        file_total_duration_seconds = get_time_in_seconds(fp.readline().rstrip())
-        part_duration_seconds = get_time_in_seconds(fp.readline().rstrip())
-
-        chapter = fp.readline().rstrip()
-
-        part_number = int(start_part_number)
-
-        while True:
-            if not chapter:
-                break
-
-            chapter_words = chapter.split(' ')
-            time_start_file = get_time_in_seconds(chapter_words[0])
-            chapter_name = '\\ '.join(chapter_words[1:])
-
-            next_chapter = fp.readline().rstrip()
-
-            if not next_chapter:
-                time_end_file = file_total_duration_seconds
-            else:
-                time_end_file = get_time_in_seconds(next_chapter.split(' ')[0])
-
-            part_number = split_file(filename, part_prefix, separator, chapter_name, time_end_file, time_start_file, part_number, part_duration_seconds)
-
-            chapter = next_chapter
-
-
-def main():
-    print('---- File splitter ----\n')
-    print('Please add your file into Files folder. The clipped files will be stored into Parts folder.\n')
-
-    should_read_parameters_file = input('Are you using the parameters file? (Y/n): ')
-
-    if(should_read_parameters_file == 'n' or should_read_parameters_file == 'N'):
-        read_inputs()
-    else:
-        read_parameters_file()
-
-
-if __name__ == "__main__":
-    main()
+        part_number = split_chapter(filename, part_prefix, separator, chapter_name, time_end_file, time_start_file, part_number, part_duration_seconds)
