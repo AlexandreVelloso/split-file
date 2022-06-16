83df56870433fd7118034562df1e475b8f7220b9
Alexandre Velloso <xande.velloso@gmail.com>
2022-06-15 23:23:23 -0300

    Creating window

--- main.py
+++ main.py
@@ -1,16 +1,41 @@
+from fileinput import filename
 import PySimpleGUI as sg
+import os.path
 
-# All the stuff inside your window.
-layout = [  [sg.Text('Hello World!')],
-            [sg.Button('Ok')] ]
+file_picker = [
+    [
+        sg.Text("Please select a file"),
+        sg.In(key="file_path", size=(30, 1), enable_events=True),
+        sg.FileBrowse(file_types=(("*.mp3"),)),
+    ]
+]
 
-# Create the Window
-window = sg.Window('Window Title', layout)
+file_viewer = [
+    [sg.Text("File name")],
+    [sg.Text(size=(100, 1), key="file_viewer")],
+]
 
-# Event Loop to process "events" and get the "values" of the inputs
+split_file_button = [
+    [sg.Button("Split file")],
+]
+
+layout = [
+    [
+        file_picker,
+        file_viewer,
+        split_file_button,
+    ]
+]
+
+window = sg.Window("Split audio file", layout)
+
+# event loop
 while True:
     event, values = window.read()
-    if event == sg.WIN_CLOSED or event == 'Ok': # if user closes window or clicks cancel
+    if event == sg.WIN_CLOSED or event == "Exit":
         break
+    if event == "file_path":
+        filename = values["file_path"]
+        window['file_viewer'].update(filename)
 
 window.close()
