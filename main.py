de23d3edbc84fdb914f32450a25118879910d223
Alexandre Velloso <xande.velloso@gmail.com>
2022-09-22 23:46:22 +0100

    .

--- main.py
+++ main.py
@@ -14,6 +14,7 @@ class MainMenu:
     
     def __init__(self, root):
         self.split_file = None
+        self.is_stopped = False
 
         self.form_width = 1100
         self.form_height = 350
